a167c8f46bb8f94b9d87df1297ec5161394050bf
endarmuh <2141720160@student.polinema.ac.id>
2023-05-31 11:11:31 +0700

    Praktikum 1 P13 - Login

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiAuthController;
 use App\Http\Controllers\MahasiswaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -24,3 +25,5 @@ Route::get('/hello', function() {
     // return response()->json($data);
 });
 Route::apiResource('/mahasiswa',MahasiswaController::class);
+
+Route::post('/login', [ApiAuthController::class,'login']);
